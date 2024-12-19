dd1b233e31ec4be5e0cfc27c34a398bf117c5c9b
Trever Fuhrer <clevertrever31@gmail.com>
2024-12-18 21:35:19 -0600

    fart

--- Calculator/main.py
+++ Calculator/main.py
@@ -18,7 +18,8 @@ print(fNum,getSign(opp),sNum, " = ", result)
 go = input("use -1 to stop or hit any key to continue ")
 
 def fart():
-    print("Fart")
+    while(True):
+        print("Fart")
 
 if(go != "-1"):
     while(True): 
@@ -30,8 +31,6 @@ if(go != "-1"):
         result = doMath(opp,result,nxtNum)
         print(oldResult,getSign(opp),nxtNum, " = ", result)
         go2 = input("use -1 to stop or hit any key to continue")
-        fart()
-        fart()
         fart()
         if(go2 == "-1"):
             break
